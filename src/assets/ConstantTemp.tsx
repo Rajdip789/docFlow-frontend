d9465bf539f68a7ee9de7d4ea072074c8842d366
rajdip <palrajdip33@gmail.com>
2024-02-05 22:19:36 +0530

    pushed assets

---
+++ src/assets/ConstantTemp.tsx
@@ -0,0 +1,105 @@
+// Template 1
+import CreateImg from "/plus.png";
+import BrochureImg from "@/assets/Brochure.png";
+import LetterImg from "@/assets/Letter.png";
+import ProjectImg from "@/assets/Project.png";
+import ReportImg from "@/assets/Report.png";
+import ResumeImg from "@/assets/Resume.png";
+
+// CV Images
+import cv1img from "@/assets/cvTemps/cv1.png";
+import cv2img from "@/assets/cvTemps/cv2.png";
+import cv3img from "@/assets/cvTemps/cv3.png";
+import cv4img from "@/assets/cvTemps/cv4.png";
+import cv5img from "@/assets/cvTemps/cv5.png";
+
+// Letter Images
+import letter1img from "@/assets/letterTemps/letter1.png";
+import letter2img from "@/assets/letterTemps/letter2.png";
+import letter3img from "@/assets/letterTemps/letter3.png";
+import letter4img from "@/assets/letterTemps/letter4.png";
+
+// Personal Images
+import personal1img from "@/assets/personalTemps/personal1.png";
+import personal2img from "@/assets/personalTemps/personal2.png";
+
+// workTemps Images
+import work1img from "@/assets/workTemps/work1.png";
+import work2img from "@/assets/workTemps/work2.png";
+import work3img from "@/assets/workTemps/work3.png";
+import work4img from "@/assets/workTemps/work4.png";
+import work5img from "@/assets/workTemps/work5.png";
+import work6img from "@/assets/workTemps/work6.png";
+import work7img from "@/assets/workTemps/work7.png";
+import work8img from "@/assets/workTemps/work8.png";
+
+// educational Images
+import education1img from "@/assets/educationTemps/education1.png";
+import education2img from "@/assets/educationTemps/education2.png";
+import education3img from "@/assets/educationTemps/education3.png";
+import education4img from "@/assets/educationTemps/education4.png";
+import education5img from "@/assets/educationTemps/education5.png";
+import education6img from "@/assets/educationTemps/education6.png";
+import education7img from "@/assets/educationTemps/education7.png";
+import education8img from "@/assets/educationTemps/education8.png";
+import education9img from "@/assets/educationTemps/education9.png";
+
+export const templateDetails = [
+	[
+		{ img: CreateImg, name: "Blank Document", subtitile: "" },
+		{ img: BrochureImg, name: "Brochure", subtitile: "Geometric" },
+		{ img: ResumeImg, name: "Resume", subtitile: "Serif" },
+		{ img: LetterImg, name: "Letter", subtitile: "Spearmint" },
+		{ img: ProjectImg, name: "Project Proposal", subtitile: "Tropic" },
+		{ img: ReportImg, name: "Report", subtitile: "Luxe" },
+	],
+	// CV
+	[
+		{ img: cv1img, name: "Resume", subtitile: "Swiss" },
+		{ img: cv2img, name: "Resume", subtitile: "Coral" },
+		{ img: cv3img, name: "Resume", subtitile: "Serif" },
+		{ img: cv4img, name: "Resume", subtitile: "Spearmint" },
+		{ img: cv5img, name: "Resume", subtitile: "Mordern Writer" },
+	],
+	[
+		{ img: letter1img, name: "Letter", subtitile: "Spearmint" },
+		{ img: letter2img, name: "Letter", subtitile: "Swiss" },
+		{ img: letter3img, name: "Business letter", subtitile: "Geometric" },
+		{ img: letter4img, name: "Informal letter", subtitile: "Plum" },
+	],
+	[
+		{ img: personal1img, name: "Recipe", subtitile: "Coral" },
+		{ img: personal2img, name: "Pet resume", subtitile: "Speralmint" },
+	],
+	[
+		{ img: work1img, name: "Project proposal", subtitile: "Spearmint" },
+		{ img: work2img, name: "Project proposal", subtitile: "Swiss" },
+		{ img: work3img, name: "Meeting notes", subtitile: "Geometric" },
+		{ img: work4img, name: "Brochure", subtitile: "Plum" },
+		{ img: work5img, name: "Brochure", subtitile: "Spearmint" },
+		{ img: work6img, name: "Newsletter", subtitile: "Swiss" },
+		{ img: work7img, name: "Newsletter", subtitile: "Geometric" },
+		{ img: work8img, name: "Consulting agreement", subtitile: "Plum" },
+	],
+	[
+		{ img: education1img, name: "Essay", subtitile: "Playful" },
+		{ img: education2img, name: "Essay", subtitile: "Playful" },
+		{ img: education3img, name: "Report", subtitile: "Geometric" },
+		{ img: education4img, name: "Report", subtitile: "Playful" },
+		{ img: education5img, name: "Report", subtitile: "Luxe" },
+		{ img: education6img, name: "Class note", subtitile: "Luxe" },
+		{ img: education7img, name: "Class note", subtitile: "Paper back" },
+		{ img: education8img, name: "Lesson plan", subtitile: "Playful" },
+		{ img: education9img, name: "Lesson plan", subtitile: "Paper back" },
+	],
+	
+	// save documents  just demo
+	[
+		{ img: BrochureImg, name: "Brochureszdxfghjhcxfgh", subtitile: "23-06-2023" },
+		{ img: ResumeImg, name: "Resvfxghjvbume", subtitile: "06-01-2024" },
+		{ img: LetterImg, name: "Letter", subtitile: "19-10-2023" },
+		{ img: ProjectImg, name: "Project Proposal", subtitile: "21-01-2024" },
+		{ img: ReportImg, name: "Report", subtitile: "01-08-2023" },
+		{ img: education7img, name: "Class note", subtitile: "Paper back" },
+	],
+];
